2f632bdd5bef33c5a81493c19a9ec81dc0e50b0e
Martin Honnen <martin.honnen@gmx.de>
2024-07-06 14:28:42 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml;
+using System.Xml.Schema;
+using System.Xml.XPath;
+
+var xmlSchemaSet = new XmlSchemaSet();
+xmlSchemaSet.Add(null, "schema1.xsd");
+
+var xmlReaderSettings = new XmlReaderSettings()
+{
+    ValidationType = ValidationType.Schema,
+    Schemas = xmlSchemaSet
+};
+
+using var xmlReader = XmlReader.Create("sample1.xml", xmlReaderSettings);
+
+var xmlDoc = new XmlDocument();
+
+xmlDoc.Load(xmlReader);
+
+var xpathNav = xmlDoc.CreateNavigator();
+
+var leafElements = xpathNav.Select("//*[not(*)]");
+
+foreach (XPathNavigator leafElement in leafElements)
+{
+    Console.WriteLine($"{leafElement.NodeType} {leafElement.Name} has typed value |{leafElement.TypedValue}| of type {leafElement.TypedValue.GetType()}");
+}
